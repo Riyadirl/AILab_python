330582d8c0ad7f95b9cb5aa546a42833e03efc9b
Mahbub Hasaan Riyad <riyad.info1@gmail.com>
2023-03-22 01:43:50 +0600

    .

---
+++ 6_list.py
@@ -0,0 +1,54 @@
+
+'''list1 = [1, 2, 3]
+list2 = [1, "hello", [2, 3, 4], 5.4]
+print(list1)
+print(list2)
+
+
+for i in range(len(list2)):
+    print(list2[i])
+
+
+for i in list2:
+    print(i)
+'''
+
+
+'''list1 = [5, 8, 7, 2, -6, 9, 0]
+
+print(list1[2:6])
+print(list1[:3])  # list1[:3]
+print(list1[4:])  # list1[4:]
+
+
+output =
+[7, 2, -6, 9]
+[5, 8, 7]
+[-6, 9, 0]
+'''
+
+'''
+list1 = [5, 8, 7, 2, -6, 9, 0]
+print(list1)
+list1.append(59)
+print(list1)
+list1.insert(2, 99)
+print(list1)
+list1.remove(7)  # value
+print(list1)
+# list1.remove()
+
+list1.sort()
+print(list1)
+
+print(list1[-3])
+print(list1[2:-3])
+
+output:
+[5, 8, 7, 2, -6, 9, 0]
+[5, 8, 7, 2, -6, 9, 0, 59]
+[5, 8, 99, 7, 2, -6, 9, 0, 59]
+[5, 8, 99, 2, -6, 9, 0, 59]
+[-6, 0, 2, 5, 8, 9, 59, 99]
+9
+[2, 5, 8]'''
---
+++ 7_function.py
@@ -0,0 +1,14 @@
+"""def square(x):
+    return x*x
+
+
+y = square(10)
+print(y)
+
+
+"""
+
+
+def printInfo(name, age=-1, id=0):
+    print("name: " + name, "age: " + age, "id: " + id)
+    printInfo("ms asad", 24)
