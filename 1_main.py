c3ef857508ac613d296b852814802f18593940ff
Mahbub Hasaan Riyad <riyad.info1@gmail.com>
2023-03-22 00:28:29 +0600

    .

--- 1_main.py
+++ 1_main.py
@@ -1,10 +1,14 @@
-#print("Mahbub hasan riyad")
-#print("Riyad" , 1234, 56.63)
+# print("Mahbub hasan riyad")
+# print("Riyad" , 1234, 56.63)
 
-#console input
+# console input
 
-x=int(input())
-y= int(input())
+x = int(input())
+y = int(input())
 
-print(x,y)
+print(x, y)
 print(x-y)
+
+name = (input("Enter your name"))
+age = int(input("enter your age"))
+print("name")
---
+++ 3_striing.py
@@ -0,0 +1,11 @@
+name1 = 'mahbub'
+name2 = 'Riayd'
+print(name1)
+print(name2)
+
+#f string
+
+name3 = 'mahbub'
+age= 23
+formstr =f"{name3} is aged {age} years old"
+print(formstr)
