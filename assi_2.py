bcdfdf176d856cb7ec15c159872edf7548296429
Mahbub Hasaan Riyad <riyad.info1@gmail.com>
2023-03-25 18:50:42 +0600

    2nd assignment

--- assi_2.py
+++ assi_2.py
@@ -1,4 +1,110 @@
+'''Problem: Sorting a list in ascending order with only right shift
+Algorithm: Hill Climbing (Steepest ascent)
+
+#Initialize():
+	initialize a list -> [7, 1, 9, 0, 5, 8, 4, 2, 10, 0, 20] and return it
+
+#calculate_cost(state):
+Counting Inversion Problem
+for each element of the list:
+look forward in the list and see how many elements are smaller than this element i.e. how many are in wrong order	
+Add up the number of disorders and return
+	
+#State_generation(current_state):
+	while True:
+current_state_cost = calculate_cost(current_state)
+print(current_state, current_state_cost )
+min_next_cost = INF
+min_next_state = None
+for each element in the list:
+swap with the forward elements of the list with this element one by one and generate one state for each swap using a for loop.
+	next_state = newly generated state by shifting the element right n times
+next_state_cost = calculate_cost(next_state)
+
+if next_state_cost is smaller than min_next_cost:
+min_next_cost = next_state_cost
+min_next_state = next_state 
+
+# take that state which has the smallest cost
+if min_next_cost is smaller than current_state_cost:
+	current_state = min_next_state
+else :
+	print(“Final State:”, current_state, current_state_cost )
+break
+
+#main():
+state = Initialize()
+State_generation(state)
+FINISH
+
+write this code with comments
+Sure, here's the code with comments:
+
+python
+Copy code
 # Initialize the starting state by creating a list with some random integers
+def initialize():
+    state = [7, 1, 9, 0, 5, 8, 4, 2, 10, 0, 20]
+    return state
+
+# Define a function to calculate the cost of the current state
+# The cost is calculated using the number of inversions in the list
+def calculate_cost(state):
+    cost = 0
+    for i in range(len(state)):
+        for j in range(i+1, len(state)):
+            if state[i] > state[j]:
+                cost += 1
+    return cost
+
+# Define a function to generate new states by swapping elements and shifting to the right
+def state_generation(current_state):
+    while True:
+        # Calculate the cost of the current state
+        current_state_cost = calculate_cost(current_state)
+        print("Current state: ", current_state, "Cost: ", current_state_cost)
+        
+        # Initialize variables to keep track of the minimum cost and state
+        min_next_cost = float('inf')
+        min_next_state = None
+        
+        # Generate all possible next states by swapping each element with its next neighbor
+        for i in range(len(current_state)-1):
+            next_state = current_state.copy()
+            # Swap the elements
+            next_state[i], next_state[i+1] = next_state[i+1], next_state[i]
+            # Shift the list to the right
+            next_state = [next_state[-1]] + next_state[:-1]
+            # Calculate the cost of the next state
+            next_state_cost = calculate_cost(next_state)
+            
+            # Update the minimum cost and state if necessary
+            if next_state_cost < min_next_cost:
+                min_next_cost = next_state_cost
+                min_next_state = next_state
+        
+        # If the minimum cost is less than the current cost, move to the next state
+        # Otherwise, print the final state and cost and exit the loop
+        if min_next_cost < current_state_cost:
+            current_state = min_next_state
+        else:
+            print("Final state: ", current_state, "Cost: ", current_state_cost)
+            break
+
+# Define the main function to initialize the state and generate new states
+def main():
+    state = initialize()
+    state_generation(state)
+
+# Call the main function to start the program
+main()
+Hope that helps!
+
+'''
+
+# Initialize the starting state by creating a list with some random integers
+
+
 def initialize():
     state = [7, 1, 9, 0, 5, 8, 4, 2, 10, 0, 20]
     return state
