a97854c399dded1a4bca94bd9d08113c55e92484
Mahbub Hasaan Riyad <riyad.info1@gmail.com>
2023-04-29 11:59:02 +0600

    .

--- 14_Plotting.py
+++ 14_Plotting.py
@@ -1,10 +1,67 @@
+import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib import pyplot as plt
 
-x = np.array([0.5, 1, 2, 2, 2, 3.5, 3.75, 4.25])
+x = np.array([0.5, 1, 1.5, 2, 2.5, 3.5, 3.75, 4.25])
 y = x**3
 
 plt.scatter(x, y)
 plt.plot(x, y)
 
+plt.show()
+
+
+# Line Plot: Plotting a line graph of a mathematical function.
+
+x = np.linspace(0, 2*np.pi, 100)
+y = np.sin(x)
+
+plt.plot(x, y)
+plt.xlabel("x")
+plt.ylabel("sin(x)")
+plt.title("Sine Wave")
+plt.show()
+
+
+# Scatter Plot: Plotting a scatter plot of random data.
+
+x = np.random.rand(50)
+y = np.random.rand(50)
+colors = np.random.rand(50)
+sizes = 1000*np.random.rand(50)
+
+plt.scatter(x, y, c=colors, s=sizes, alpha=0.5)
+plt.xlabel("X-axis")
+plt.ylabel("Y-axis")
+plt.title("Scatter Plot")
+plt.show()
+
+
+# Histogram: Plotting a histogram of a sample datasett
+
+data = np.random.randn(1000)
+
+plt.hist(data, bins=30, alpha=0.5)
+plt.xlabel("Value")
+plt.ylabel("Frequency")
+plt.title("Histogram of Random Data")
+plt.show()
+
+# Bar Chart: Plotting a bar chart of a sample dataset.
+x = np.array(['A', 'B', 'C', 'D', 'E'])
+y = np.array([3, 5, 1, 7, 4])
+
+plt.bar(x, y)
+plt.xlabel("Category")
+plt.ylabel("Value")
+plt.title("Bar Chart")
+plt.show()
+
+
+# Pie Chart: Plotting a pie chart of a sample dataset.
+x = np.array([25, 40, 10, 25])
+labels = ['A', 'B', 'C', 'D']
+
+plt.pie(x, labels=labels, autopct='%1.1f%%')
+plt.title("Pie Chart")
 plt.show()
